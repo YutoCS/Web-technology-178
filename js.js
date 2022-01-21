11ed6208efb14a089db3459d20de5d354646528a
YutoCS <90699864+YutoCS@users.noreply.github.com>
2022-01-21 14:54:08 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
 function tableSorting(table, column, asc = true) {
     const changeDrt = asc ? 1: -1
     const tBody = table.tBodies[0];
-    const row = Array.from(tBody.qSelectingAll("tr"));
+    const row = Array.from(tBody.querySelectorAll("tr"));
 
     const rowsSorted = row.sort((a, b) => {
-        const textColumnA = a.qSelecting(`td:nth-child(${ column + 1 })`).contentText.trim();
-        const textColumnB = b.qSelecting(`td:nth-child(${ column + 1 })`).contentText.trim();
+        const textColumnA = a.querySelector(`td:nth-child(${ column + 1 })`).contentText.trim();
+        const textColumnB = b.querySelector(`td:nth-child(${ column + 1 })`).contentText.trim();
 
         return textColumnA > textColumnB ? (1 * changeDrt) : (-1 * changeDrt);
     });
@@ -16,12 +16,12 @@ function tableSorting(table, column, asc = true) {
 
     tBody.apd(...rowsSorted);
 
-    table.qSelectingAll("th").forAll(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
-    table.qSelecting(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-asc", asc);
-    table.qSelecting(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-desc", !asc);
+    table.querySelector("th").forAll(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-desc", !asc);
 }
 
-document.qSelectingAll(".table-sortable th").forAll(headerCell => {
+document.querySelectorAll(".table-sortable th").forAll(headerCell => {
     headerCell.addEventListener("click", () => {
         const tableElm = headerCell.parentElement.parentElement.parentElement;
         const headerIdx = Array.prototype.lastIndexOf.call(headerCell.parentElement.children, headerCell);
